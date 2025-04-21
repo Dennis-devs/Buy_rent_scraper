20bdda7161d02f8bf8b3a6f2d085eb9d57bd7b3a
Dennis Ogol <dennis.ogoll@gmail.com>
2025-04-21 09:08:16 +0300

    Update Buy_rent.py

--- Buy_rent.py
+++ Buy_rent.py
@@ -4,7 +4,8 @@ import csv
 import pandas as pd
 
 base_url="https://www.buyrentkenya.com/houses-for-rent"
-agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+# Add user agent here
+agent = ""
 
 
 #input your headers here
